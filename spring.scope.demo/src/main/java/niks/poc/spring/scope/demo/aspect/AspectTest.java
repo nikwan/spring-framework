c8b15c479ef0c71ebfd8c73a8876acc8a115c029
nikwan <nikhil.courser@gmail.com>
2021-07-05 19:09:42 +0530

    Aspect demo added

--- spring.scope.demo/src/main/java/niks/poc/spring/scope/demo/aspect/AspectTest.java
+++ spring.scope.demo/src/main/java/niks/poc/spring/scope/demo/aspect/AspectTest.java
@@ -2,11 +2,13 @@ package niks.poc.spring.scope.demo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
+@Component
 @Aspect
 public class AspectTest {
 	
-	@Before("public void niks.poc.spring.scope.demo.beans.Student.getName()")
+	@Before("execution(public String niks.poc.spring.scope.demo.beans.Student.getName(..))")
 	public void studentAware() {
 		System.out.println("########################before student get name##############");
 	}
